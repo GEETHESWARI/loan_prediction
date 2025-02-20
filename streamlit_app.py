f1c49fccd458b06e789581509e5a1ce93d9268ee
GEETHESWARI <121534207+GEETHESWARI@users.noreply.github.com>
2025-02-20 16:20:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('Loan Prediction')
+st.info(' This Machine learning Model predicts whether Loan will be aproved or Rejected')
 
 
