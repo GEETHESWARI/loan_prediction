466c7ac8464144ecf2d7083bd05a173f4a686dd9
GEETHESWARI <121534207+GEETHESWARI@users.noreply.github.com>
2025-02-20 18:30:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,13 @@ st.info(' This Machine learning Model predicts whether Loan will be aproved or R
 with st.expander('Data'):
   st.write("**Weather data**")
   loan=pd.read_csv("https://raw.githubusercontent.com/GEETHESWARI/loan_prediction/refs/heads/master/loan_approval_dataset.csv")
-  loan
+  
   loan.columns=loan.columns.str.strip()
   loan.columns
+  loan["Assets"]=loan.residential_assets_value+loan.commercial_assets_value+loan.luxury_assets_value+loan.bank_asset_value
+  loan.drop(columns=['residential_assets_value',
+       'commercial_assets_value', 'luxury_assets_value', 'bank_asset_value'],inplace=True)
+  loan
   
   st.write('**x data-Input features**') 
   x_raw = loan.drop(['loan_status','loan_id'], axis=1)
@@ -34,6 +38,9 @@ with st.expander('Data Visualization'):
     st.bar_chart(loan, x="self_employed", y="loan_amount", color="loan_status", stack=False)
   with tab3:  
    st.bar_chart(loan, x='loan_term', y='cibil_score', color="loan_status")
+
+
+
   
 
 
