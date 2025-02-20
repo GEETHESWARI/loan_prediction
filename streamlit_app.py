678f7ec910119a9d487d906462b9de84a6a0aaba
GEETHESWARI <121534207+GEETHESWARI@users.noreply.github.com>
2025-02-20 16:26:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,9 @@ from sklearn.ensemble import RandomForestClassifier
 st.title('Loan Prediction')
 st.info(' This Machine learning Model predicts whether Loan will be aproved or Rejected')
 
+with st.expander('Data'):
+  st.write("**Weather data**")
+  loan=pd.read_csv("https://raw.githubusercontent.com/GEETHESWARI/loan_prediction/refs/heads/master/loan_approval_dataset.csv")
+  loan
+
 
